ae82f8ead8e9c20bf9162bb22bd906053c5807b4
Kartik Bahuguna <bahugunakartik10@gmail.com>
2026-08-14 01:25:20 +0530

    Day 5

---
+++ Day 5/10.c
@@ -0,0 +1,17 @@
+// Write a program to input time in seconds and convert it to hours:minutes:seconds format.
+
+#include <stdio.h>
+int main() {
+    int seconds, hours, minutes, remainingSeconds;
+    printf("Enter time in seconds: ");
+    scanf("%d", &seconds);
+
+    // Conversion
+    hours = seconds / 3600;
+    minutes = (seconds % 3600) / 60;
+    remainingSeconds = seconds % 60;
+
+
+    printf("Time = %d:%d:%d (hours:minutes:seconds)\n", hours, minutes, remainingSeconds);
+    return 0;
+}
