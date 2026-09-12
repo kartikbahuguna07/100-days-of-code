a39792c8261cc9f48724305bf885a6982bb2db25
Kartik Bahuguna <bahugunakartik10@gmail.com>
2026-09-12 22:24:39 +0530

    Day 34

---
+++ Day 34/67.c
@@ -0,0 +1,36 @@
+// Insert an element in an array at a given position.
+
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, pos, val, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter position to insert (1 to %d): ", n+1);
+    scanf("%d", &pos);
+
+    printf("Enter value to insert: ");
+    scanf("%d", &val);
+
+    
+    for(i = n; i >= pos; i--) {
+        arr[i] = arr[i-1];
+    }
+
+    arr[pos-1] = val; // Insert at given position
+    n++; 
+
+    printf("Array after insertion:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
