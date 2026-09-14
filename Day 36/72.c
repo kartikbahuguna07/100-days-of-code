1088ee722f16458e5c7e214fc978716dd3136c09
Kartik Bahuguna <bahugunakartik10@gmail.com>
2026-09-14 21:29:43 +0530

    Day 36

---
+++ Day 36/72.c
@@ -0,0 +1,37 @@
+// Find the sum of all elements in a matrix.
+
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
+    int sum = 0;
+
+    // Reading the matrix
+    printf("Enter elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+            sum += matrix[i][j];  // add each element to sum
+        }
+    }
+
+    // Printing the matrix
+    printf("The matrix is:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Printing the sum
+    printf("Sum of all elements = %d\n", sum);
+
+    return 0;
+}
