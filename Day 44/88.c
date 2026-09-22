1f1257a88b1a584238304b591f32bf7718aa842b
Kartik Bahuguna <bahugunakartik10@gmail.com>
2026-09-22 21:52:10 +0530

    Day 44

---
+++ Day 44/88.c
@@ -0,0 +1,23 @@
+//  Replace spaces with hyphens in a string.
+
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    while(str[i] != '\0')
+    {
+        if(str[i] == ' ')
+            str[i] = '-';
+
+        i++;
+    }
+
+    printf("Modified string: %s", str);
+
+    return 0;
+}
